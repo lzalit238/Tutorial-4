0293d8e64dfa12c29b8aad2468251474aea7808b
lzalit238 <lzalit238@west-mec.org>
2021-10-13 14:04:01 -0700

    Upload useContext Folder

---
+++ cra-template-lzalit/template/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+      <h3>Basic React Template</h3>
+    </div>
+  );
+}
+
+export default App;
